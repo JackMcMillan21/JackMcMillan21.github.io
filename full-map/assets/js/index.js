dbdb2fd1823fac27f45c8c7306a444561b34a827
JackMcMillan21 <78222906+JackMcMillan21@users.noreply.github.com>
2022-04-19 13:48:33 -0500

    Map

---
+++ full-map/assets/js/index.js
@@ -0,0 +1,28 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFja21jbWlsbGFuMjEiLCJhIjoiY2wyNTRkZ2JzMDNyMzNrbGpxdDJhcXUwYiJ9.9xryIEzpvPEdyZxTq1VVsg';
+
+
+function showLocation(position){
+    const {longitude, latitude} = position.coords;
+    console.log(`Longitude: ${longitude}`);
+    console.log(`Latitude: ${latitude}`);
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [longitude, latitude], // starting position [lng, lat]
+        zoom: 12 // starting zoom
+    });
+}
+
+//The 'failure' callback function
+function errorHandler(){
+console.log("nope");
+}
+
+if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showLocation, errorHandler, {enableHighAccuracy:true});
+}else{
+        console.log(`Geolocation is not supported b thr browser`);
+}
+
+showLocation();
